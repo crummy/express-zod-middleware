9821bce8fe5ef9f1000e883ef403b149298161ba
crummy <crummynz@gmail.com>
2023-11-15 11:49:44 +1300

    Revert "Attempt to genericise with functions"
    This reverts commit 25dd1547ce9039ab6fce770ce8f88b63c82c877a.

--- src/broken.ts
+++ src/broken.ts
@@ -1,30 +1,26 @@
 import {createMiddleware, defaultEndpointsFactory} from "express-zod-api";
-import {z} from "zod";
+import { z } from "zod";
 
-function createMiddlewareA<IN extends z.ZodObject<any, z.UnknownKeysParam>, OPT, OUT extends Record<string, any> & { id: string}, SCO extends string>() {
-    return createMiddleware<IN, OPT, OUT, SCO>({
-        input: z.object({}),
-        middleware: async () => {
-            return {
-                id: "an id",
-            }
-        },
-    })
-}
+const middlewareA = createMiddleware({
+    input: z.object({}),
+    middleware: async () => {
+        return {
+            id: "an id",
+        }
+    },
+})
 
-function createMiddlewareB<IN extends z.ZodObject<any, any, any>, OPT, OUT extends Record<string, any>, SCO extends string>() {
-    return createMiddleware<IN, {id: string}, OUT, SCO>({
-        input: z.object({}),
-        middleware: async ({options}) => {
-            console.log(`this does not work because options is "any": ${options.id}`)
-            return {
-                ...options,
-                something: "else",
-            }
-        },
-    });
-}
+const middlewareB = createMiddleware({
+    input: z.object({}),
+    middleware: async ({ options }) => {
+        console.log(`this does not work because options is "any": ${options.id}`)
+        return {
+            ...options,
+            something: "else",
+        }
+    },
+})
 
 const brokenExample = defaultEndpointsFactory
-    .addMiddleware(createMiddlewareA())
-    .addMiddleware(createMiddlewareB())
+    .addMiddleware(middlewareA)
+    .addMiddleware(middlewareB)
